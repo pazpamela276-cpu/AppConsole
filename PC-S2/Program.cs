7e43555df07bc8904cd1d45ce5d61fdb816ee6a0
pazpamela276-cpu <paz>
2026-04-15 21:26:25 -0500

    cambio para conflicto

---
+++ PC-S2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VERSION DESDE MAIN");
+
+            Console.Write("Ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine($"Bienvenido, {nombre} 👋");
+
+            Console.WriteLine("Presiona una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
